d031b28cb36d569ba0fcdce74a15b4da8aa6dea9
Joseph McEvoy <joseph_mce@live.co.uk>
2025-12-15 09:20:09 -0500

    Created create functionality

--- app.py
+++ app.py
@@ -14,10 +14,26 @@ transactions = [
 # Read operation
 @app.route("/")
 def get_transactions():
-    
-
+    return render_template("transactions.html", transactions=transactions)
 
 # Create operation
+@app.route("/add", methods=["GET", "POST"])
+def add_transactions():
+    #Check if the request method is POST from the form submission
+    if request.method =='POST':
+        #Then create a new transaction obhect using the form field values
+        transaction = {
+            'id': len(transactions) + 1, #Generate a new ID based on the current length of the transactions list
+            'date': request.form['date'], #Get the 'date' field value from the form
+            'amount': float(request.form['amount']) #Get the 'amount' field value from the form and convert it to a float
+        }
+        #Append the new transaction to the transactions list
+        transactions.append(transaction)
+        #Redirect to the transactions list page after adding the new transaction
+        return redirect(url_for("get_transactions"))
+    
+    #If the request method is GET, render the form template to display the add transaction form
+    return render_template("form.html")
 
 # Update operation
 
